b2aca34459fcdd6098b795bf9ab9f94117352db4
James Thompson <keiouu@gmail.com>
2012-07-03 21:24:29 +0100

    Move some things around, some bootstrap work

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * This file is passed a desired page, it then serves up that page by any means necessary!
+ */
+
+// Make sure we have a page!
+if (!isset($_GET['sdt-page'])) {
+	$page = "/403.html";
+} else {
+	$page = $_GET['sdt-page'];
+}
+
+// If this URL doesnt have a file name, add index.html
+if (strlen($page) <= 0 || substr($page, -1)) {
+	$page = $page . "index.html";
+}
+
+// See if this page is cached
+print $page;
